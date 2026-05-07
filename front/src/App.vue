39195cfce940b1c2a97911f4a8b446a0d4c44a43
= <=>
2026-05-07 16:12:14 +0300

    Initialisation projet mini-crm

---
+++ front/src/App.vue
@@ -0,0 +1,13 @@
+<script setup></script>
+
+<template>
+  <h1 class="text-3xl text-blue-500 font-bold">
+    Tailwind fonctionne !
+  </h1>
+  <p>
+    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
+    documentation
+  </p>
+</template>
+
+<style scoped></style>
